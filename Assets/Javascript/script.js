1a15ab4ea73d726e9492b5a8cecfa2aaf57dd6a6
TJ Elder <telder55@gmail.com>
2021-01-16 17:01:50 -0700

    Added validation

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -16,29 +16,69 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // Function to prompt user choices
 
 function generatePassword () {
-    var passwordLength = prompt("Enter a number between 8-128"); 
+    var passwordLength = prompt("Enter a number between 8-128");   
+    var password = "";
+
     if (passwordLength < 8) {
         alert("number is too small");
-        return;
+        return "Click Generate Password Button to Try Again";
     }
     if (passwordLength > 128) {
         alert("number is too large");
-        return;
+        return "Click Generate Password Button to Try Again";
+    }
+    if (isNaN (passwordLength)) {
+        alert("must be a number between 8-128");
+        return "Click Generate Password Button to Try Again";
     }
     var lowercaseChoice = confirm("Include lowercase letters?")
+    if (lowercaseChoice === true) {
+        passwordSelection = [...passwordSelection, ...lowercase];
+    }
+
+    else {
+        passwordSelection = [...passwordSelection]
+    }
 
     var uppercaseChoice = confirm("Include uppercase letters?")
+    if (uppercaseChoice === true) {
+        passwordSelection = [...passwordSelection, ...uppercase];
+    }
+    else {
+        passwordSelection = [...passwordSelection]
+    }
+
 
     var numbersChoice = confirm("Include numbers?")
-    
-    var specialcharChoice = confirm("Include special characters?")
-    
+    if (numbersChoice === true) {
+        passwordSelection = [...passwordSelection, ...numbers];
 
+    }
+    else {
+        passwordSelection = [...passwordSelection]
+    }
+
+    var specialcharChoice = confirm("Include special characters?")
+    if (specialcharChoice === true) {
+        passwordSelection = [...passwordSelection, ...specialchar];
+    }
+
+    else {
+        passwordSelection = [...passwordSelection]
+    }
+
+
+    for (var i=0; i < passwordLength; i++) {
+        password = password + passwordSelection [(Math.floor(Math.random() * passwordSelection.length))]
+        
+    }
+    return password
 
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
